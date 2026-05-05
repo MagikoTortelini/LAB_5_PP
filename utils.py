f091c84b35c8fdce28125e6dae8c51c9488acc9e
kacpaw2195 <k.pawlakq@gmail.com>
2026-05-05 18:05:50 +0200

    Poprawki

--- utils.py
+++ utils.py
@@ -1,9 +1,23 @@
-# Exemplary calculator functions
-def add (a: int , b: int) -> int :
+"""
+Exemplary calculator functions
+"""
+
+
+def add(a: int, b: int) -> int:
+    """DODAWANIE"""
     return a + b
-def subtract (a: int , b: int) -> int :
+
+
+def subtract(a: int, b: int) -> int:
+    """ODEJMOWANIE"""
     return a - b
-def multiply (a: int , b: int) -> int :
+
+
+def multiply(a: int, b: int) -> int:
+    """MNOŻENIE"""
     return a * b
-def divide ( a: int , b : int ) -> float :
+
+
+def divide(a: int, b: int) -> float:
+    """DZIELENIE"""
     return a / b
